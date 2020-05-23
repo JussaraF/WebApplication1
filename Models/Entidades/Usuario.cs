39fa6c607c01fe43f7f553143f319d037cd8707d
JussaraF <65784148+JussaraF@users.noreply.github.com>
2020-05-23 20:13:48 -0300

    Add files via upload

---
+++ Models/Entidades/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Entidades
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Description = "Codigo")]
+        public int Id { get; set; }
+        [Display(Description = "Nome do usuario")]
+        public string NomeUsuario { get; set; }
+        [Display(Description = "Idade")]
+        public int Idade { get; set; }
+        [Display(Description = "Tipo de usuario")]
+        public int Tipo { get; set; }
+    }
+}
